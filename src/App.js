e0d052d5a5cfeeea66c14dbf8da877678da782ea
q-miyata <hikarumiyata@hikarunoMacBook-Pro.local>
2024-05-24 15:34:32 +0900

    second commit

--- src/App.js
+++ src/App.js
@@ -10,11 +10,14 @@ function Square({ value,onSquareClick }) {
 
 function Board({ xIsNext, squares, onPlay }) {
 
+  
 function handleClick(i) {
   if (squares[i] || calculateWinner(squares)){
     return;
   }
   const nextSquares = squares.slice();
+  //ひとつ前の配列のコピーを作ってクリックされた部分の[i]を書き換えている
+  //nextsquaresは配列
   if(xIsNext){
     nextSquares[i] = "X";
   } else{
@@ -54,20 +57,31 @@ if (winner) {
   );
 }
 
-export default function Game(){
-  const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = useState([Array(9).fill(null)]);
-  const currentSquares = history[history.length -1];
 
+export default function Game(){
+
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  //const currentSquares = history[history.length -1];
+  const currentSquares = history[currentMove];
+
+  //新しい配列（その手での配列（nextsquares）を今までの履歴に足している）
   function handlePlay(nextSquares){
-    setHistory([...history, nextSquares]);
-    setXIsNext(!xIsNext);
+    // setHistory([...history, nextSquares]);
+    // setXIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1); //戻った手以降の履歴を消している。
+
   }
 
   function jumpTo(nextMove){
-
+    //currentMoveで指定した手に戻る
+      setCurrentMove(nextMove);　
   }
 
+  //map メソッド　第一引数に配列の要素、第二にインデックスを取る
   const moves = history.map((squares, move) => {
     let description;
     if (move > 0){
@@ -76,7 +90,7 @@ export default function Game(){
       description = 'Go to game start';
     }
     return (
-      <li>
+      <li key={move}>
         <button onClick={() => jumpTo(move)}>{description}</button>
       </li>
     )
@@ -109,6 +123,7 @@ function calculateWinner (squares) {
   ];
   for (let i = 0; i < lines.length; i++){
     const [a,b,c] = lines[i];
+    console.log("as ",i,squares[a],squares[b],squares[c]);
     if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
       return squares[a];
     }
